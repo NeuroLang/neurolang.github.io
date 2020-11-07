59dfb3939c5d4ff0cb736aeec105a1fdf5a9b408
Circle CI <circle@neurolang.com>
2020-11-07 12:02:12 +0000

    Non-tagged commit updating dev doc (5927).

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev459+gfc921e8',
+    VERSION: '0.1.dev462+g93ae84d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
